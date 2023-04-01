7d59c32819a63d6a8ef10fb93e34f5024e6c2bf8
Engincan KÖSE <kosengincan@gmail.com>
2023-04-01 14:27:06 +0300

    now user can save their favorite pages!

--- wiki/views.py
+++ wiki/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse, redirect
 import wikipedia
 from django.contrib.auth.decorators import login_required
+from .models import SavedSearch
 
 # Create your views here.
 @login_required(login_url='')
@@ -14,3 +15,17 @@ def wiki(request):
             return HttpResponse("Wrong Input")
         return render(request, "main/index.html", {"result": result})
     return render(request, "main/index.html")
+
+@login_required(login_url='')
+def save_search(request):
+    if request.method == "POST":
+        query = request.POST['query']
+        result = request.POST['result']
+        user = request.user
+        SavedSearch.objects.create(query=query, result=result, user=user)
+        return redirect('archives')
+
+@login_required(login_url='')
+def archives(request):
+    searches = SavedSearch.objects.filter(user=request.user).order_by('-created_at')
+    return render(request, "main/archives.html", {"searches": searches})
