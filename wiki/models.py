77714faebb44c72ca69addbc57e73da6225463f8
Engincan KÖSE <kosengincan@gmail.com>
2023-04-04 22:20:53 +0300

    timestamp and visual improvements added

--- wiki/models.py
+++ wiki/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class SavedSearch(models.Model):
     query = models.CharField(max_length=600)
